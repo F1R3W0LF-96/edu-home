c8431abf3c9b96023306cb84984840c274d3c15b
rozerAdda247 <bag.rozer@adda247.com>
2023-04-21 12:24:12 +0530

    the rozer dev

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.materialdesignicons.com/6.5.95/css/materialdesignicons.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
