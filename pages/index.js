52c300c0ee9c417393b579ce6aa88cf9597cade2
Ankit-digimantra <ankit.parashar@digimantra.com>
2023-05-03 00:38:56 +0530

    "change for login"

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import LandingPage from "@/components/LandingPage/LandingPage";
 import Wrapper from "@/components/Layouts/Wrapper";
 import Teams from "@/components/Teams/Teams";
 import Contact from "@/components/Contact/Contact";
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (localStorage) {
+      if (localStorage.getItem("accessToken")) {
+        push("/student");
+      }
+    }
+  }, []);
+
   return (
     <Wrapper>
       <LandingPage />
