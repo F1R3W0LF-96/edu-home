e102f22bbee2ffadabf4f2c50983cc799fdfee70
Ankit-digimantra <ankit.parashar@digimantra.com>
2023-05-13 15:03:45 +0530

    "cahnges for location  and login"

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,16 +5,22 @@ import logo from "../../public/Images/logo.png";
 import Avatar from "../Elements/Avatar";
 import { Button } from "antd";
 import { useRouter } from "next/router";
+import coinImage from "../../public/Images/rupee.png";
 
-const Header = ({ isAuthenticated, os }) => {
+const Header = ({ isAuthenticated, os, location }) => {
   const { push } = useRouter();
   const mobileMenu = useRef(null);
   const [openMenu, setOpenMenu] = useState(false);
+  const [userName, setUserName] = useState("");
   const [loggedInUser, setLoggedInUser] = useState(false);
+  const [showProfileDropdown, setShowProfileDropdown] = useState(false);
   useEffect(() => {
     if (localStorage) {
       if (localStorage.getItem("accessToken")) {
         setLoggedInUser(true);
+      }
+      if (localStorage.getItem("fullName")) {
+        setUserName(localStorage.getItem("fullName"));
       }
     }
   }, []);
@@ -83,6 +89,7 @@ const Header = ({ isAuthenticated, os }) => {
               }
             />
           )}
+
           <button className="navbar-burger flex items-center text-blue-600 p-3">
             <svg
               className="block h-4 w-4 fill-current"
@@ -103,15 +110,75 @@ const Header = ({ isAuthenticated, os }) => {
               }
             />
           )}
+
           {loggedInUser ? (
-            <Button
-              onClick={() => {
-                localStorage.removeItem("accessToken");
-                push("/");
-              }}
-            >
-              Logout
-            </Button>
+            <>
+              <div class="relative">
+                <button
+                  id="dropdownDefaultButton"
+                  data-dropdown-toggle="dropdown"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  type="button"
+                  onClick={() => setShowProfileDropdown(!showProfileDropdown)}
+                >
+                  {userName}(
+                  <Image src={coinImage} alt={"rupee"} width={20} />
+                  {1000} coins)
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    aria-hidden="true"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
+                  </svg>
+                </button>
+
+                <div
+                  id="dropdown"
+                  class={`z-10 ${
+                    showProfileDropdown
+                      ? "opacity-100 pointer-events-auto"
+                      : "opacity-0 pointer-events-none"
+                  } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute right-0 transition-opacity duration-200`}
+                >
+                  <ul
+                    class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownDefaultButton"
+                  >
+                    <li>
+                      <a
+                        onClick={() => {
+                          push("/profile");
+                        }}
+                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Profile
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        onClick={() => {
+                          localStorage.removeItem("accessToken");
+                          push("/");
+                        }}
+                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Sign out
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </>
           ) : (
             <>
               <Link
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,41 @@ import LandingPage from "@/components/LandingPage/LandingPage";
 import Wrapper from "@/components/Layouts/Wrapper";
 import Teams from "@/components/Teams/Teams";
 import Contact from "@/components/Contact/Contact";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+import locationImg from "../public/Images/location.png";
 
 export default function Home() {
   const { push } = useRouter();
+  const [currentLocation, setCurrentLocation] = useState([]);
+
+  const [location, setLocation] = useState();
+
+  const fetchApiData = async ({ latitude, longitude }) => {
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+    );
+    const data = await res.json();
+    debugger;
+    if (data.address) setCurrentLocation(data.address);
+  };
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
+        setLocation({ latitude, longitude });
+      });
+    }
+  }, []);
+  useEffect(() => {
+    // Fetch data from API if `location` object is set
+    if (location) {
+      fetchApiData(location);
+    }
+  }, [location]);
 
   useEffect(() => {
     if (localStorage) {
@@ -17,10 +47,36 @@ export default function Home() {
   }, []);
 
   return (
-    <Wrapper>
-      <LandingPage />
-      <Teams />
-      <Contact />
-    </Wrapper>
+    <>
+      <div className="flex justify-end">
+        {currentLocation && (
+          <>
+            <span
+              style={{
+                // maxWidth: "200px",
+                marginRight: "20px",
+                wordBreak: "break-word",
+                display: "flex",
+                whiteSpace: "nowrap",
+                fontSize: "12px",
+                // overflow: "hidden !important",
+                alignItems: "center",
+
+                // textOverflow: "ellipsis",
+              }}
+              title={`${currentLocation?.suburb},${currentLocation?.town}`}
+            >
+              <Image src={locationImg} width={20} alt="logo" />
+              {currentLocation.suburb},{currentLocation.town}
+            </span>
+          </>
+        )}
+      </div>
+      <Wrapper>
+        <LandingPage />
+        <Teams />
+        <Contact />
+      </Wrapper>
+    </>
   );
 }
